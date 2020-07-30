d3bf21ad46c2f49be00a615003a00f48808342cb
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-07-30 14:54:29 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rafaelgdn:rafa2404@cluster0.g2zm8.mongodb.net/clean-node-api?retryWrites=true&w=majority',
+  port: process.env.port || 5050
+}
