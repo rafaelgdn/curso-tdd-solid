37f9dc99404387db98ec1fb057c7c7efe5661113
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-06 13:56:01 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rafaelgdn:rafa2404@cluster0.g2zm8.mongodb.net/clean-node-api?retryWrites=true&w=majority',
-  port: process.env.port || 5050
+  port: process.env.port || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'wps2#beTFsD'
 }
