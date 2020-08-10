13374d00c4bfa5e2b5990a5525158a50965cf1e6
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-10 17:00:26 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,11 +1,11 @@
 import { makeLoginValidation } from './login-validation-factory'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validators/validation'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { ValidationComposite } from '../../../../presentation/helpers/validators/validation-composite'
+import { RequiredFieldValidation } from '../../../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../../../presentation/helpers/validators/validation'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { EmailValidation } from '../../../../presentation/helpers/validators/email-validation'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
