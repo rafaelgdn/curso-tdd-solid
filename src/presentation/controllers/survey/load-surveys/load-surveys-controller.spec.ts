24003c178a5195a8aada1641ce0a7eeb8dc7f340
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-28 17:10:26 -0300

    test: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -2,6 +2,7 @@ import { LoadSurveysController } from './load-surveys-controller'
 import { SurveyModel } from '../../../../domain/models/survey'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import MockDate from 'mockdate'
+import { ok } from '../../../helpers/http-helper'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [{
@@ -60,5 +61,10 @@ describe('LoadSurveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveysStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()))
   })
 })
