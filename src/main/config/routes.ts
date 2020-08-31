fe36024a60308909a9bc9610a69e552893d15191
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-31 11:40:08 -0300

    fix: make controllers return the error message instead of the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,16 +1,11 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-// import fg from 'fast-glob'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  //   fg.sync('**/src/main/routes/**routes.ts')
-  //     .map(async file => {
-  //       (await import(`../../../${file}`)).default(router)
-  //     })
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
