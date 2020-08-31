bb183fd3cd077413c14e908c082f20a8a337fff8
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-31 14:27:31 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct id

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -1,6 +1,6 @@
 import { LoadSurveys } from '../../../domain/usecases/load-surveys'
 import { SurveyModel } from '../../../domain/models/survey'
-import { LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository copy'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
 
 export class DbLoadSurveys implements LoadSurveys {
   constructor (
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,7 +1,7 @@
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository copy'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
 import { SurveyModel } from '../../../../domain/models/survey'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
